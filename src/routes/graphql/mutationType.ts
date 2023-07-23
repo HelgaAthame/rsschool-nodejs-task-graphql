268d68809606f83aa5207372e299bc101392a27d
HelgaAthame <athamethedarkest@gmail.com>
2023-07-23 17:41:47 +0300

    create delete mutations

--- src/routes/graphql/mutationType.ts
+++ src/routes/graphql/mutationType.ts
@@ -1,7 +1,20 @@
-import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  GraphQLBoolean,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
 import { postType, profileType, userType } from './types/types.js';
 import { createPostInput, createProfileInput, createUserInput } from './inputs.js';
-import { createPost, createProfile, createUser } from './resolvers.js';
+import {
+  createPost,
+  createProfile,
+  createUser,
+  deletePost,
+  deleteProfile,
+  deleteUser,
+} from './resolvers.js';
+import { UUIDType } from './types/uuid.js';
 
 const createMutationFields = {
   createPost: {
@@ -33,9 +46,34 @@ const createMutationFields = {
   },
 };
 
+export const deleteMutationFields = {
+  deletePost: {
+    type: GraphQLBoolean,
+    args: {
+      id: { type: new GraphQLNonNull(UUIDType) },
+    },
+    resolve: async (_source, args) => await deletePost(args),
+  },
+  deleteUser: {
+    type: GraphQLBoolean,
+    args: {
+      id: { type: new GraphQLNonNull(UUIDType) },
+    },
+    resolve: async (_source, args) => await deleteUser(args),
+  },
+  deleteProfile: {
+    type: GraphQLBoolean,
+    args: {
+      id: { type: new GraphQLNonNull(UUIDType) },
+    },
+    resolve: async (_source, args) => await deleteProfile(args),
+  },
+};
+
 export const mutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     ...createMutationFields,
+    ...deleteMutationFields,
   }),
 });
