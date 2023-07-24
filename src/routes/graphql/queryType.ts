7edd2eddb7fb3f634344713bc65aee0b06a1d74e
olga <khmarukolgaivanovna@gmail.com>
2023-07-24 09:37:03 +0300

    feat: create profile queries

--- src/routes/graphql/queryType.ts
+++ src/routes/graphql/queryType.ts
@@ -1,9 +1,11 @@
 import { GraphQLObjectType } from "graphql";
 import { userQueries } from './queries/userQueries.js';
+import { profileQueries } from './queries/profileQueries.js';
 
 export const queryType = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: () => ({
     ...userQueries,
+    ...profileQueries,
     }),
   });
