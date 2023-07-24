933fe922788d21bc48a02fc710b33cfcdfee3e2e
olga <khmarukolgaivanovna@gmail.com>
2023-07-24 09:35:58 +0300

    feat: create user queries

--- src/routes/graphql/queryType.ts
+++ src/routes/graphql/queryType.ts
@@ -1,7 +1,9 @@
 import { GraphQLObjectType } from "graphql";
+import { userQueries } from './queries/userQueries.js';
 
 export const queryType = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: () => ({
+    ...userQueries,
     }),
   });
