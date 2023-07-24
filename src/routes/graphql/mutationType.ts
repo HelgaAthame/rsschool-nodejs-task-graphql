5896e8814c2dfc9679265cc394f58b03e8509068
olga <khmarukolgaivanovna@gmail.com>
2023-07-24 09:41:23 +0300

    feat: create post mutations

--- src/routes/graphql/mutationType.ts
+++ src/routes/graphql/mutationType.ts
@@ -1,9 +1,11 @@
 import { GraphQLObjectType } from 'graphql';
 import { userMutations } from './queries/userMutations.js';
+import { postMutations } from './queries/postMutations.js';
 
 export const mutationType = new GraphQLObjectType({
   name: 'RootMutationType',
   fields: () => ({
     ...userMutations,
+    ...postMutations,
   }),
 });
